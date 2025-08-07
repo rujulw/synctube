870461f6b95ae6d4e82a9731cf4d3068bb0f0074
rpwaval <rpwaval@ncsu.edu>
2025-08-08 00:45:35 +0530

    initialize backend with socket.io

---
+++ server/index.js
@@ -0,0 +1,31 @@
+// server/index.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on('video-event', ({ roomId, event }) => {
+    socket.to(roomId).emit('video-event', event);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Socket.IO server running on port 3001');
+});
